548c5a69121796b52e98aec28688dc7d250ff753
JoJeHun <cho9759@gmail.com>
2023-07-10 14:44:13 +0900

    문자열을 파싱하는 테스트 구현

--- src/test/java/study/tdd/StringCalculator.java
+++ src/test/java/study/tdd/StringCalculator.java
@@ -1,11 +1,16 @@
 package study.tdd;
 
+import java.util.List;
+
 public class StringCalculator {
 
     private final String text;
+    private final StringParser stringParser = new StringParser();
 
     public StringCalculator(String text) {
         this.text = text;
+        StringParser stringParser = new StringParser();
+        List<String> parseText = stringParser.parse(text);
     }
 
     public String getText() {
--- src/test/java/study/tdd/StringCalculatorTest.java
+++ src/test/java/study/tdd/StringCalculatorTest.java
@@ -3,6 +3,8 @@ package study.tdd;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class StringCalculatorTest {
 
     @Test
@@ -12,5 +14,17 @@ public class StringCalculatorTest {
         StringCalculator stringCalculator = new StringCalculator(input);
 
         Assertions.assertThat(stringCalculator.getText());
+    }
+
+    @Test
+    public void 문자열을_파싱한다() {
+        String input = "2 + 3 * 4 / 2";
+
+        StringParser stringParser = new StringParser();
+
+        List<String> parser = stringParser.parse(input);
+        List<String> stringList = List.of("2", "+", "3", "*", "4", "/", "2");
+
+        Assertions.assertThat(parser).isEqualTo(stringList);
     }
 }

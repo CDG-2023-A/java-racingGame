8505e85b5c16334f9c914c58c856180065c8a463
wellbeing-dough <kdw990202@naver.com>
2023-07-11 17:21:15 +0900

    test : 문자열 입력 테스트 코드

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,49 @@
 package study;
 
+import calculator.DongwooCalculator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
-    @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+
+    private static DongwooCalculator dwCalculator;
+    private static StringParser stringParser;
+    private static final String parsingErrormessage = "올바른 계산식을 입력해주세요.\n";
+
+    @BeforeAll
+    static void init() {
+        dwCalculator = new DongwooCalculator();
+        stringParser = new StringParser();
     }
+
+    @Test
+    void 숫자를_입력_받는다() {
+        String input = "1 + 2 * 3";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        stringParser.parse(input);
+        Assertions.assertThat(input + "\n").isEqualTo(out.toString());
+    }
+
+    @Test
+    void 숫자를_입력_받는다_실패() {
+
+        String input = "1 + 2 a 3";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        stringParser.parse(input);
+        Assertions.assertThat(parsingErrormessage).isEqualTo(out.toString());
+
+    }
+
+
 }
